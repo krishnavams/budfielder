1cf3dfee98f1eee6b546c047f46b1a04dc1bbfb2
krishnavams <35889193+krishnavams@users.noreply.github.com>
2022-03-12 09:30:05 +0530

    changes excel

--- BudFielder/src/com/budfielder/genaric/FileLib.java
+++ BudFielder/src/com/budfielder/genaric/FileLib.java
@@ -13,4 +13,10 @@ public class FileLib extends BaseClass {
 		return WorkbookFactory.create(fis).getSheet(SheetName).getRow(RowNumber).getCell(CellNumber)
 				.getStringCellValue();
 	}
+	public void setExtranalData(String SheetName, int RowNumber, int CellNumber, String SetValue)
+			throws EncryptedDocumentException, IOException {
+		FileInputStream fis = new FileInputStream("./Support/GenaricData1.xlsx");
+		WorkbookFactory.create(fis).getSheet(SheetName).getRow(RowNumber).getCell(CellNumber)
+				.setCellValue(SetValue);
+	}
 }
